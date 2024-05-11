25f0894f02a4c29f6ff194e4eba7591faa3451f9
plusplusgame <sharplusplus@gmail.com>
2024-05-11 11:49:50 +0900

    UEプロジェクト作成

---
+++ Source/PlaPlaUESample/PlaPlaUESampleGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PlaPlaUESampleGameMode.generated.h"
+
+UCLASS(minimalapi)
+class APlaPlaUESampleGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	APlaPlaUESampleGameMode();
+};
+
+
+
